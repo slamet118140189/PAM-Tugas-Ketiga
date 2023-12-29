e07ab5987e12ed1d49ca4dabc0ef6da65b2c3b2a
slamet118140189 <slamet.118140189@student.itera.ac.id>
2023-12-30 06:06:18 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
     }
 }
